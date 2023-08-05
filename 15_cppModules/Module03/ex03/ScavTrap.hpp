f16bc9d78639f0088248f2bd05d7f579730f1222
arbutnar <butnaruariannaa@gmail.com>
2023-08-05 17:28:47 +0200

    Module03 complete

---
+++ 15_cppModules/Module03/ex03/ScavTrap.hpp
@@ -0,0 +1,10 @@
+#include "ClapTrap.hpp"
+
+class ScavTrap: virtual public ClapTrap {
+	public:
+		ScavTrap( void );
+		ScavTrap(std::string name);
+		~ScavTrap( void );
+		void attack (const std::string &target);
+		void guardGate();
+};
