2a73feeb580c6b33bc5ae16d0cc2c8e46256e8b6
arbutnar <butnaruariannaa@gmail.com>
2023-09-06 15:13:08 +0200

    ls

--- 15_cppModules/Module07/ex01/iter.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-template <typename T>
-void	display(T element);
-
-template <typename T1, typename T2, typename Func>
-void	iter(T1 *array, T2 len, Func display);
